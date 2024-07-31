1e68a688ecd46dfe83ed3cf3d74399586405d55e
Alexsander Lopes <alexsanderlaureanolopes@gmail.com>
2024-07-31 08:33:36 -0300

    feat: adicionando validações com o yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,9 +1,29 @@
 import { Request, Response } from "express";
 import { ICidade } from "../../model/entities/ICidade";
+import * as yup from "yup";
 
+//VALIDAR AS INFORMAÇÕES RECEBIDAS NO BODY DA REQUISIÇÃO
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
+
+//CRIAÇÃO DA CIDADE
 // eslint-disable-next-line @typescript-eslint/ban-types
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-    console.log(req.body.nome);
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validatedData: ICidade | undefined = undefined;
+    try {
+        validatedData = await bodyValidation.validate(req.body);
+
+        console.log(validatedData);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+            },
+        });
+    }
 
     return res.send("Cidade criada!");
 };
